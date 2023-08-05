b659c1dcac4b5d932d885a94cf5969cc02aed312
Fritz <fritzj96@gmail.com>
2023-08-04 20:33:46 -0400

    further validation comments

--- src/reservation/validation.ts
+++ src/reservation/validation.ts
@@ -5,5 +5,5 @@ import { z } from "zod";
 
 export const restaurantSearchSchema = z.object({
   dateTime: z.date().min(new Date()), // We don't want to allow reservations in the past.
-  party: z.array(z.string().cuid()).nonempty(),
+  party: z.array(z.string().cuid()).nonempty(), // We don't want to allow empty parties.
 });
