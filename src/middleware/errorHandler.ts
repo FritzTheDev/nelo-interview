8d29a236d0ca0ed7604626c94400b6a68ac72c21
Fritz <fritzj96@gmail.com>
2023-08-04 20:25:05 -0400

    error handling middleware for zod

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,25 @@
+// middleware/error.ts
+// This file is where we'll write our global error handling middleware.
+// All errors will be passed to this middleware, and it will be responsible for sending the appropriate response.
+// We're only specifically handling validation errors here, but we could handle any error type we want.
+
+/**
+ * Express error handling middleware.
+ * Handles ZodError validation errors specifically.
+ * Returns a generic error message for everything else.
+ */
+export function errorHandler(err: any, _req: any, res: any, _next: any) {
+  if (err.name === "ZodError") {
+    res.status(400).json({
+      message: "Validation failed.",
+      errors: err.errors.map((error: any) => {
+        const { path, message } = error;
+        return { path, message };
+      }),
+    });
+  } else {
+    res.status(500).json({
+      message: "An unexpected error occurred.",
+    });
+  }
+}
