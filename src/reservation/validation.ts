1bb4496a019ae986419bfae07a27ea233e692b5a
Fritz <fritzj96@gmail.com>
2023-08-04 20:33:23 -0400

    added a check for past reservations

--- src/reservation/validation.ts
+++ src/reservation/validation.ts
@@ -4,6 +4,6 @@
 import { z } from "zod";
 
 export const restaurantSearchSchema = z.object({
-  dateTime: z.date(),
+  dateTime: z.date().min(new Date()), // We don't want to allow reservations in the past.
   party: z.array(z.string().cuid()).nonempty(),
 });
