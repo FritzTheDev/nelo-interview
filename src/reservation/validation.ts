1fb4cdebabfdb665aa5069594cbf08575cc2dba1
Fritz <fritzj96@gmail.com>
2023-08-06 17:11:47 -0400

    property name fixes continue

--- src/reservation/validation.ts
+++ src/reservation/validation.ts
@@ -11,5 +11,5 @@ export const restaurantSearchSchema = z.object({
 export const createReservationSchema = z.object({
   dateTime: z.date({ coerce: true }).min(new Date()), // We don't want to allow reservations in the past.
   party: z.array(z.string().cuid()).nonempty(), // We don't want to allow empty parties.
-  restaurantId: z.string().cuid(),
+  restaurant: z.string().cuid(),
 });
