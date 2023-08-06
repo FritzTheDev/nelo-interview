31c3d430a0d2a9cc5987ba3279be717f07119020
Fritz <fritzj96@gmail.com>
2023-08-06 17:01:37 -0400

    jest-ts & typescript config

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+// jest.config.ts
+// This file is used to configure Jest.
+
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
+
+export default config;
